26b2cf80817fcd11c3a8dd6624301c4a3bfb9b5e
Julia Gu <Juejing.Gu@yh.nackademin.se>
2019-09-03 13:04:27 +0200

    pull master

---
+++ src/test/java/pac/FortuneTellerTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pac;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Julia
+ */
+public class FortuneTellerTest {
+    
+    FortuneTeller fortuneTeller = FortuneTeller.getInstance();
+    
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
